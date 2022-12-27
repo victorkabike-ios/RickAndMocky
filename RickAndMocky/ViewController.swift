a7283b4295dee6952ac99d82ee1eb440b07521a5
Victor Kabike <tinotendakab@gmaail.com>
2022-12-27 08:41:02 +0200

    Initial Commit

---
+++ RickAndMocky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMocky
+//
+//  Created by victor kabike on 2022/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
